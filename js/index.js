9a7a4691b6d2224143bcbb565440c6f51e741873
Steven Ambrose <93626591+steambro01@users.noreply.github.com>
2023-10-18 10:35:23 -0500

    transfered this sober tracker project from my chromebook

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.onload=function(){
+    //when submit button for calender is clicked
+    document.getElementById("calenderSubmit").addEventListener("click",function(){
+        //alert pops up saying congratulations
+        alert("Congratulations!");
+    });
+};
